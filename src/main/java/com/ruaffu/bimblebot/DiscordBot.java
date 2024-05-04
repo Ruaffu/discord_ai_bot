906eb44cd89dbace6cccf11956b505833a66d329
Ruaffu <78867945+Ruaffu@users.noreply.github.com>
2024-05-04 16:28:33 +0200

    Major refactor of discord bot moved chatgpt methods to its own service

--- src/main/java/com/ruaffu/bimblebot/DiscordBot.java
+++ src/main/java/com/ruaffu/bimblebot/DiscordBot.java
@@ -1,115 +1,75 @@
 package com.ruaffu.bimblebot;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
-import jakarta.annotation.PostConstruct;
-import lombok.extern.slf4j.Slf4j;
+import java.util.regex.*;
+
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.stereotype.*;
+
+import com.ruaffu.bimblebot.service.*;
+
+import lombok.extern.slf4j.*;
+
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.requests.GatewayIntent;
-import org.apache.http.HttpHeaders;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
-import org.springframework.web.reactive.function.BodyInserters;
-import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
-
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 @Slf4j
 @Component
 public class DiscordBot extends ListenerAdapter {
-    @Value("${discord.bot.token}")
-    private String botToken;
+	@Value("${discord.bot.token}")
+	private String botToken;
+	private final ChatGptService chatGptService;
 
-    @Value("${openai.api.key}")
-    private String openAiApiKey;
+	@Autowired
+	public DiscordBot(ChatGptService chatGptService) {
+		this.chatGptService = chatGptService;
+	}
 
-    @Value("${discordbot.url}")
-    private String url;
+	public void startBot() throws Exception {
+		JDABuilder.createDefault(botToken)
+				.addEventListeners(this)
+				.enableIntents(GatewayIntent.MESSAGE_CONTENT)
+				.build();
+	}
 
-    private WebClient webClient;
+	@Override
+	public void onMessageReceived(MessageReceivedEvent event) {
 
-    @PostConstruct
-    public void init(){
-        webClient = createWebClient();
-    }
+		if (event.getAuthor().isBot())
+			return;
 
-    private WebClient createWebClient() {
-        return WebClient.builder()
-                .baseUrl(url)
-                .defaultHeader(HttpHeaders.AUTHORIZATION, "Bearer " + openAiApiKey)
-                .defaultHeader(HttpHeaders.CONTENT_TYPE, "application/json")
-                .build();
-    }
-    public void startBot() throws Exception {
-        JDABuilder.createDefault(botToken)
-                .addEventListeners(this)
-                .enableIntents(GatewayIntent.MESSAGE_CONTENT)
-                .build();
-    }
+		log.debug("User : {} asked a question", event.getAuthor().getName());
 
-    @Override
-    public void onMessageReceived(MessageReceivedEvent event) {
+		String message = event.getMessage().getContentRaw();
 
-        if (event.getAuthor().isBot()) return;
+		log.debug("message: {}", message);
+		if (message.contains(event.getJDA().getSelfUser().getAsMention())) {
+			String messageContent = event.getMessage().getContentRaw();
 
-        log.debug("User : {} asked a question",event.getAuthor().getName());
+			// Define the regex pattern for matching user/bot mentions
+			Pattern mentionPattern = Pattern.compile("<@!?(\\d+)>");
 
-        String message = event.getMessage().getContentRaw();
+			// Create a matcher object for the message content
+			Matcher mentionMatcher = mentionPattern.matcher(messageContent);
 
-        log.debug("message: {}",message);
-        if (message.contains(event.getJDA().getSelfUser().getAsMention())) {
-            String messageContent = event.getMessage().getContentRaw();
+			// Remove all user/bot mentions from the message content
+			String trimmedMessage = mentionMatcher.replaceAll("");
 
-            // Define the regex pattern for matching user/bot mentions
-            Pattern mentionPattern = Pattern.compile("<@!?(\\d+)>");
+			// Trim leading and trailing whitespace
+			trimmedMessage = trimmedMessage.trim();
 
-            // Create a matcher object for the message content
-            Matcher mentionMatcher = mentionPattern.matcher(messageContent);
+			chatGptService.sendMessageToChatGPT(trimmedMessage)
+					.subscribe(resp -> {
+								// Send the response back to the Discord channel
+								event.getChannel().sendMessage(resp).queue();
+							},
+							error -> {
+								log.error("An error occurred while generating the response.", error);
+								event.getChannel().sendMessage("An error occurred while generating the response.").queue();
+							});
+		}
 
-            // Remove all user/bot mentions from the message content
-            String trimmedMessage = mentionMatcher.replaceAll("");
+	}
 
-            // Trim leading and trailing whitespace
-            trimmedMessage = trimmedMessage.trim();
-
-            generateChatGPTResponse(trimmedMessage)
-                    .subscribe(resp -> {
-                                // Send the response back to the Discord channel
-                                event.getChannel().sendMessage(resp).queue();
-                            },
-                            error -> {
-                                log.error("An error occurred while generating the response.", error);
-                                event.getChannel().sendMessage("An error occurred while generating the response.").queue();
-                            });
-        }
-
-    }
-
-    private Mono<String> generateChatGPTResponse(String message) {
-            log.info("Question: {}",message);
-            String requestBody = String.format("{\"model\": \"gpt-3.5-turbo\", \"messages\": [{\"role\": \"user\", \"content\": \" %s\"}]}", message);
-            return webClient.post()
-                    .body(BodyInserters.fromValue(requestBody))
-                    .retrieve()
-                    .bodyToMono(String.class)
-                    .map(responseBody -> {
-                        Gson gson = new Gson();
-                        JsonObject responseJson = gson.fromJson(responseBody, JsonObject.class);
-
-                        // Extract the "content" value
-                        String content = responseJson.getAsJsonArray("choices")
-                                .get(0).getAsJsonObject()
-                                .get("message").getAsJsonObject()
-                                .get("content").getAsString();
-
-                        log.debug("response : {}",content);
-
-                        return content;
-                    }).onErrorReturn("An error occurred while generating the response.");
-
-    }
 }
